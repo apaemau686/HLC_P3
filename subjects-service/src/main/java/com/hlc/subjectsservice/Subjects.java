8e4d18a9552452b608c0ce1e5e300eff8ad1ff74
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-01-08 12:27:59 +0100

    Añadidos los proyectos

---
+++ subjects-service/src/main/java/com/hlc/subjectsservice/Subjects.java
@@ -0,0 +1,9 @@
+package com.hlc.subjectsservice;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import java.util.List;
+
+public interface Subjects {
+    @GetMapping("/subjects")
+    List<String> getAllSubjects();
+}
