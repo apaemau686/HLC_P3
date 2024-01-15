2b6ff65892ec5a74df26f4aaed292c738d15bab8
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-01-15 12:04:23 +0100

    students y subjects se conectan con exito a la db

---
+++ students-service/src/main/java/com/hlc/studentsservice/StudentRepository.java
@@ -0,0 +1,9 @@
+package com.hlc.studentsservice;
+
+import org.springframework.data.mongodb.repository.MongoRepository;
+
+import java.util.List;
+
+public interface StudentRepository extends MongoRepository<Student, String> {
+    List<Student> findAll();
+}
